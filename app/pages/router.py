81a7a79a5681181c7cc28d7578f89eec7ac730e9
Artem_M <web.ep4@gmail.com>
2025-06-28 13:41:09 +0300

    Фронт

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Depends, UploadFile
 from fastapi.templating import Jinja2Templates
 
-from app.student.router import get_all_students
+from app.student.router import get_all_students, get_student_by_id
 
 router = APIRouter(prefix='/pages', tags=['Фронтенд'])
 templates = Jinja2Templates(directory='app/templates')
@@ -18,4 +18,15 @@ async def get_students_html(request: Request, students=Depends(get_all_students)
             'request': request,
             'students': students,
         },
+    )
+
+
+@router.get('/students/{id}')
+async def get_students_html(request: Request, student=Depends(get_student_by_id)):
+    return templates.TemplateResponse(
+        name='student.html',
+        context={
+            'request': request,
+            'student': student,
+        }
     )
