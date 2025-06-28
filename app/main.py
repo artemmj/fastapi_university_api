84ee317559b12f620966ba2c45159f53c4eb62f9
Artem_M <web.ep4@gmail.com>
2025-06-28 21:24:49 +0300

    Работа над апи

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.file.router import router as files_router
 from app.major.router import router as majors_router
@@ -8,10 +9,16 @@ from app.student.router import router as students_router
 from app.user.router import router as users_router
 
 app = FastAPI()
+templates = Jinja2Templates(directory='app/templates')
 
 @app.get("/")
-def home_page():
-    return {"message": "Привет, мир!"}
+async def reg_page(request: Request):
+    return templates.TemplateResponse(
+        name='registration.html',
+        context={
+            'request': request,
+        },
+    )
 
 app.mount('/static', StaticFiles(directory='app/static'), 'static')
 
--- app/student/router.py
+++ app/student/router.py
@@ -12,7 +12,7 @@ async def get_all_students(request_body: RBStudent = Depends()):
     return await StudentDAO.get_all(**request_body.to_dict())
 
 
-@router.get(path="/{id}", summary="Получить по айди", response_model=StudentSchemaGet | dict)
+@router.get("/{id}", summary="Получить по айди", response_model=StudentSchemaGet | dict)
 async def get_student_by_id(id: int):
     result = await StudentDAO.get_by_id(id)
     if not result:
